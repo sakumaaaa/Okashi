cf44deda5a134e29508578f887112d6821ac27f6
sakumaaaa <kuma817shuu@gmail.com>
2023-05-05 09:30:01 +0900

    Initial Commit

---
+++ Okashi/OkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiApp.swift
+//  Okashi
+//
+//  Created by asaken_n44 on 2023/05/05.
+//
+
+import SwiftUI
+
+@main
+struct OkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
