c1af7e6688d47eb376bfba12ecb8b38c4e78c53b
sakumaaaa <kuma817shuu@gmail.com>
2023-05-05 09:48:08 +0900

    add SerchTextField

--- Okashi/ContentView.swift
+++ Okashi/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    //OkashiDataを参照する状態変数
+    @StateObject var okashiDataList = OkashiData()
+    //入力された文字列を保存する状態変数
+    @State var inputText = ""
+    
     var body: some View {
+        //垂直にレイアウト
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            //文字を受け取るTextFieldを表示する
+            TextField("キーワード",
+                      text:  $inputText,
+            prompt: Text("キーワードを入力してください"))
+            .onSubmit {
+                //入力完了直後に検索をする
+                okashiDataList.serachOkashi(keyword: inputText)
+            }//.onSubmit
+            //キーボードの改行を検索に変更する
+            .submitLabel(.search)
+            //上下左右に空白を空ける
+            .padding()
+        }//VStack
+    }//View
+}//ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
